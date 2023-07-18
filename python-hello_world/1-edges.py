dcaf19c5814cdc7ea0be35fb0b3f8a88aef66579
root <jaydenrishi@gmail.com>
2023-07-18 04:04:20 -0700

    1. Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[:2]
-middle_word  = word[:7]
-print("First 3 letters: {Hol}".format(word))
-print("last 2 letters: {on}".format(word))
-print("Middle word: {olberto}".format(word))
+word_last_2 = word[-2:] 
+middle_word = word[1:len(word)]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+
