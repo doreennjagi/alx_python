890a72877580dd0c20fbb5c6c9ec66625e0ee4eb
root <jaydenrishi@gmail.com>
2023-07-18 04:25:12 -0700

    5. 00...99[Fixed]

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,3 @@
 #! /usr/bin/python3
-for i in range(0, 99):
-    if i < 10:
-        print('0{}'.format(i), end=", ")
-    else:
-        print(i ,end=", ")
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
