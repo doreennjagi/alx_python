719d1963ecb77530e0a938d4838c469b17ab8ef5
doreennjagi <jaydenrishi@gmail.com>
2023-07-18 12:55:46 +0300

    python-hello_word

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3=word[:3]
-word_first_2 =word[:2]
-middle_word =word[:4]
+word_first_3 = word[:3]
+word_last_2 = word[:2]
+middle_word  = word[:4]
 print("First 3 letters: {Hol}".format(word))
 print("Last 2 letters: {on}".format(word))
 print("Middle word: {bert}".format(word))
