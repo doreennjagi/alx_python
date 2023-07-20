e871ffaaa3f8ddc8de00ad005f543d2f20c02f4a
root <jaydenrishi@gmail.com>
2023-07-20 11:54:44 -0700

    5. Prime Number Function[Fixed]

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def is_prime(number):
-    is_prime = True
-    for i in range(2,number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
-            is_prime = False
-    return is_prime
+            return False
+
+    return True
 
