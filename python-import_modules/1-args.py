d4a27a63ab405c97c1a4dae347882b0ce2a82573
doreennjagi <jaydenrishi@gmail.com>
2023-07-27 15:04:34 +0300

    python-import_modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,30 +2,32 @@ import sys
 
 def main():
     # Get the number of arguments
-    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+    num_args = len(sys.argv) # Subtract 1 to exclude the script name
 
     # Print the number of arguments
     print(f"Number of argument(s): {num_args}")
 
     if num_args == 1:
-        print("hello")  # If no arguments were passed, print ":"
+        print("1:hello")  # If no arguments were passed, print ":"
     else:
-        print("hello", end="\n\n")  # If at least one argument, print ": followed by a new line"
+        print("1:hello holberton", end="\n\n")  # If at least one argument, print ": followed by a new line"
         
         # Print each argument with its position
         for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"{i}: {arg}")
+    # Get the number of arguments
+    num_args = len(sys.argv) # Subtract 1 to exclude the script name
         
     if num_args == 2:
-        print("Holberton")  # If no arguments were passed, print ":"
+        print(" 2:Hello")  # If no arguments were passed, print ":"
     else:
-        print("Holberton", end="\n\n")  # If at least one argument, print ": followed by a new line"
+        print("2:Hello School", end="\n\n")  # If at least one argument, print ": followed by a new line"
         
         # Print each argument with its position
         for i, arg in enumerate(sys.argv[1:], start=2):
             print(f"{i}: {arg}")
 
-    if  num_args == 3:
+    if num_args == 3:
         print("School")  # If no arguments were passed, print ":"
     else:
         print("School", end="\n\n")  # If at least one argument, print ": followed by a new line"
@@ -43,23 +45,23 @@ def main():
         for i, arg in enumerate(sys.argv[1:], start=4):
             print(f"{i}: {arg}")
 
-    if num_args == 5:
-        print("battery")  # If no arguments were passed, print ":"
-    else:
-        print("battery", end="\n\n")  # If at least one argument, print ": followed by a new line"
+        if  num_args == 5:
+            print("battery")  # If no arguments were passed, print ":"
+        else:
+            print("battery", end="\n\n")  # If at least one argument, print ": followed by a new line"
         
         # Print each argument with its position
         for i, arg in enumerate(sys.argv[1:], start=5):
             print(f"{i}: {arg}")
 
-    if num_args == 6:
-        print("street")  # If no arguments were passed, print ":"
-    else:
-        print("street", end="\n\n")  # If at least one argument, print ": followed by a new line"
+        if  num_args == 6:
+            print("street")  # If no arguments were passed, print ":"
+        else:
+            print("street", end="\n\n")  # If at least one argument, print ": followed by a new line"
 
 
         # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=6):
+        for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"{i}: {arg}")
 
 if __name__ == "__main__":
---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,9 @@
+# Import the variable 'a' from variable_load_2.py
+from variable_load_2 import a
+
+def main():
+    # Print the value of 'a'
+    print("The value of 'a' is:", a)
+
+if __name__ == "__main__":
+    main()
