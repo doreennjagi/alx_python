ce1949caa2bc71f1e65d68d9db6639d787f1c023
doreennjagi <jaydenrishi@gmail.com>
2023-07-31 20:59:36 +0300

    python-import_modules

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,9 +1,6 @@
-# Import the variable 'a' from variable_load_2.py
-from variable_load_2 import a
-
-def main():
-    # Print the value of 'a'
-    print("The value of 'a' is:", a)
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    main()
+    
+    from variable_load_2 import a
+
+    print(a)
