75f9662a8281004bd8b89afa38a9025c9cdb7624
doreennjagi <jaydenrishi@gmail.com>
2023-07-31 21:13:31 +0300

    python-import_modules

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,10 +1,8 @@
+#!/usr/bin/python3
 def raise_exception():
     try:
-        # Raise a type exception with a custom message
-        raise_exception(TypeError)
+        
+        raise TypeError
     except TypeError as te:
         # Print the error message
         print("Exception raised")
-
-# Call the function to see the type exception in action
-raise_exception()
