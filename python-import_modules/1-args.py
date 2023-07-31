d838d9177ca536cc240dd912bff6dc7e9e355a7a
doreennjagi <jaydenrishi@gmail.com>
2023-07-31 20:51:06 +0300

    python-import_modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,68 +1,13 @@
-import sys
-
-def main():
-    # Get the number of arguments
-    num_args = len(sys.argv) # Subtract 1 to exclude the script name
-
-    # Print the number of arguments
-    print(f"Number of argument(s): {num_args}")
-
-    if num_args == 1:
-        print("1:hello")  # If no arguments were passed, print ":"
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("1:hello holberton", end="\n\n")  # If at least one argument, print ": followed by a new line"
-        
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
-    # Get the number of arguments
-    num_args = len(sys.argv) # Subtract 1 to exclude the script name
-        
-    if num_args == 2:
-        print(" 2:Hello")  # If no arguments were passed, print ":"
-    else:
-        print("2:Hello School", end="\n\n")  # If at least one argument, print ": followed by a new line"
-        
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=2):
-            print(f"{i}: {arg}")
-
-    if num_args == 3:
-        print("School")  # If no arguments were passed, print ":"
-    else:
-        print("School", end="\n\n")  # If at least one argument, print ": followed by a new line"
-        
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=3):
-            print(f"{i}: {arg}")
-
-    if num_args == 4:
-        print("98")  # If no arguments were passed, print ":"
-    else:
-        print("98", end="\n\n")  # If at least one argument, print ": followed by a new line"
-        
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=4):
-            print(f"{i}: {arg}")
-
-        if  num_args == 5:
-            print("battery")  # If no arguments were passed, print ":"
-        else:
-            print("battery", end="\n\n")  # If at least one argument, print ": followed by a new line"
-        
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=5):
-            print(f"{i}: {arg}")
-
-        if  num_args == 6:
-            print("street")  # If no arguments were passed, print ":"
-        else:
-            print("street", end="\n\n")  # If at least one argument, print ": followed by a new line"
-
-
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
-
-if __name__ == "__main__":
-    main()
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
