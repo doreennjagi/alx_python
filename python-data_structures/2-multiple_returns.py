b5d93261f53be29a6a402b0aa36fb2e62322884a
doreennjagi <jaydenrishi@gmail.com>
2023-08-02 12:29:49 +0300

    python-data_structures

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:  # Check if the sentence is empty (or None)
+    if not sentence:  
         return (0, None)
     else:
         return (len(sentence), sentence[0])
-print(multiple_returns("Hello")) 
-print(multiple_returns("")) 
