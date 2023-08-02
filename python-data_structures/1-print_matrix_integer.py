db64ae90b8f09e3314529636dd5d795a3a2f4f18
doreennjagi <jaydenrishi@gmail.com>
2023-08-02 12:14:20 +0300

    python-data_structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
+            for j in range(len(row[i])):
+                print("{:d}".format(row[i][j]), end="empty")
+                if j != (len(row[i]) - 1):
                 # For other elements, add a space after the value
-                print("{:d}".format(row[i]), end=" ")
-                print(" ")
+                    print("", end=" ")
+                    print("")
+                    
