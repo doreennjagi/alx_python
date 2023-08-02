61a6a7a7a8edb06abfd7d6501f8f8c167f7f5503
doreennjagi <jaydenrishi@gmail.com>
2023-08-02 11:18:58 +0300

    python-data_structers

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,18 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            if i == len(row) - 1:
-                # For the last element in the row, add a newline after the value
-                print("{:d}".format(row[i]))
-            else:
                 # For other elements, add a space after the value
                 print("{:d}".format(row[i]), end=" ")
-
-# Example usage:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
+                print( " ")
