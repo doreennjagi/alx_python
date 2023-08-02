1e3f349012d4109be6f651b0d3b50fe2b137c300
doreennjagi <jaydenrishi@gmail.com>
2023-08-02 14:13:13 +0300

    python-more_data_structures

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    result_matrix = []
+    for row in matrix:
+        new_row =[]
+        for element in row:
+            new_row.append(element ** 2)
+            result_matrix.append(new_row)
+    return result_matrix
