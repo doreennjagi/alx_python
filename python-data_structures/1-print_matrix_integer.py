6bc48c9a92e872686c2756a6a954a617943ffaf6
doreennjagi <jaydenrishi@gmail.com>
2023-08-02 11:23:50 +0300

    python-data_structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(row)):
                 # For other elements, add a space after the value
                 print("{:d}".format(row[i]), end=" ")
-                print( " ")
+                print(" ")
