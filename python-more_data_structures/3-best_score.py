b733a94c018003b396212b8d353dbbc7f2facf95
doreennjagi <jaydenrishi@gmail.com>
2023-08-03 14:56:28 +0300

    python-more_data_structures

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary)== 0:
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    my_dict= list(a_dictionary.keys())[0]
-    best_score= a_dictionary [my_dict]
-    for k, z in a_dictionary.items():
-        if z > best_score:
-            best_score=z
-            my_dict=k
-            return (my_dict)
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
