26be1e0399c4a21cd0be3d0e9552d75dfdf452d5
doreennjagi <jaydenrishi@gmail.com>
2023-08-03 14:40:45 +0300

    python-more_data_structures

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary)== 0:
         return None
+    my_dict= list(a_dictionary.keys())[0]
+    best_score= a_dictionary [my_dict]
+    for k, z in a_dictionary.items():
+        if z > best_score:
+            best_score=z
+            my_dict=k
+            return (my_dict)
