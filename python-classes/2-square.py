ac3aee2c48cab7668c2d75878108a872240dd266
doreennjagi <jaydenrishi@gmail.com>
2023-08-11 13:37:58 +0300

    python-classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,13 +4,10 @@ class mysquare:
     """ class square that defines a square"""
     def __init__(self, size=0):
         """ initializes square size of square"""
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
-    def area(self):
-        return self.__size ** 2
-mysquare = mysquare(5)
-print("Area of square:", mysquare.area())
