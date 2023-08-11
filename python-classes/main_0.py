52c542a865f8d188343f67f2c0168349f382262f
doreennjagi <jaydenrishi@gmail.com>
2023-08-11 12:44:16 +0300

    python-classes

--- python-classes/main_0.py
+++ python-classes/main_0.py
@@ -4,12 +4,3 @@ Square = __import__('0-square').Square
 my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
