aadd74a029c377dfa9b818e81e602e99ccc9afbd
doreennjagi <jaydenrishi@gmail.com>
2023-08-26 23:10:20 +0300

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status."""
-import urllib.request
+import requests
 
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if response.status_code == 200:
+    content = response.json()
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+else:
+    print(f"Request failed with status code: {response.status_code}")
