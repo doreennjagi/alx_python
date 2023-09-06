cb85f85df9f8e33f7335079ac4e80646dbad0e06
root <jaydenrishi@gmail.com>
2023-09-05 22:46:23 -0700

    Fixed

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,19 +3,14 @@
 Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
-
-
 import sys
-import urllib.parse
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    r = requests.post(url, data=value)
+    print(r.text)
 
