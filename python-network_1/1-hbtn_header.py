cb248ff002525112bc79f158bdf8be38f50cb275
root <jaydenrishi@gmail.com>
2023-09-05 22:47:50 -0700

    Fixed: 1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,13 +3,12 @@
 Usage: ./1-hbtn_header.py <URL>
 """
 import sys
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
 
